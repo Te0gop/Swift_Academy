17a8d2a5b353c80493426d97d1c87504f3774cb8
Te0gop <soulshine_@abv.bg>
2020-04-19 08:32:34 +0300

    exam

--- Final_Exam_Task/src/Main.java
+++ Final_Exam_Task/src/Main.java
@@ -4,15 +4,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Mammal cat = new Cat("Persian", true, Colors.orange, 4.3, 1);
-        Mammal baldCat = new Cat("Egyptian", 5.5, 2);
-        Mammal bigDog = new Dog("Kangal", 66.5, 3, Colors.black);
-        Fish fish = new Fish("Cat fish", 5, "everything", false, 8);
+        Animal cat = new Cat("Persian", true, Colors.orange, 4.3, 1);
+        Animal baldCat = new Cat("Egyptian", 5.5, 2);
+        Animal bigDog = new Dog("Kangal", 66.5, 3, Colors.black);
+        Animal fish = new Fish("Cat fish", 5, "everything", false, 8);
 
-        List<Mammal> list = new ArrayList<>();
+        List<Animal> list = new ArrayList<>();
         list.add(cat);
         list.add(baldCat);
         list.add(bigDog);
+        list.add(fish);
 
         for (int i = 0; i < list.size() ; i++) {
             if(list.get(i).getClass().getSimpleName().equals("Cat") && list.get(i).getWeight() < 5) {
